71f6bc04366c6aac2dadadb46274cc7e571798da
manhdez21-cpu <manhdez21@gmail.com>
2026-07-08 11:43:43 -0500

    Implement click event for sello with animations
    Added event listener for 'sello' to trigger animations and redirect.

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const invitado = invitados[codigo];
 const nombre = document.getElementById("nombre");
 const mensaje = document.getElementById("mensaje");
 const sobre = document.getElementById("sobre");
+const sello = document.getElementById("sello");
+const selloIzq = document.getElementById("selloIzq");
+const selloDer = document.getElementById("selloDer");
 
 // Si existe el invitado
 if (invitado) {
@@ -28,10 +31,43 @@ if (invitado) {
 
 }
 
-// Al tocar el sobre abre la invitación
-sobre.addEventListener("click", () => {
+sello.addEventListener("click", () => {
 
-    window.location.href =
-    "https://malauboda.my.canva.site/malauboda";
+    sello.style.display = "none";
+
+    selloIzq.style.opacity = "1";
+    selloDer.style.opacity = "1";
+
+    selloIzq.animate([
+        {
+            transform:"translate(-50%,-50%) rotate(0deg)"
+        },
+        {
+            transform:"translate(-90px,-30px) rotate(-28deg)"
+        }
+    ],{
+        duration:350,
+        fill:"forwards",
+        easing:"ease-out"
+    });
+
+    selloDer.animate([
+        {
+            transform:"translate(-50%,-50%) rotate(0deg)"
+        },
+        {
+            transform:"translate(40px,-70px) rotate(30deg)"
+        }
+    ],{
+        duration:350,
+        fill:"forwards",
+        easing:"ease-out"
+    });
+
+    setTimeout(()=>{
+
+        window.location.href="https://malauboda.my.canva.site/malauboda";
+
+    },350);
 
 });
