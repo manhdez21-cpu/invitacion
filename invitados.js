f677ff205615151f4a9a23237320838e48f14dba
manhdez21-cpu <manhdez21@gmail.com>
2026-07-07 22:06:37 -0500

    Add invitados data structure with guest details

--- invitados.js
+++ invitados.js
@@ -1,1 +1,138 @@
+const invitados = {
 
+"001":{
+nombre:"Luz Marina Marín",
+cupos:1
+},
+
+"002":{
+nombre:"Fernando Rivera",
+cupos:1
+},
+
+"003":{
+nombre:"Jose Manuel Rivera",
+cupos:1
+},
+
+"004":{
+nombre:"Alejandra Camacho",
+cupos:3
+},
+
+"005":{
+nombre:"Adriana Camacho",
+cupos:4
+},
+
+"006":{
+nombre:"Socorro",
+cupos:2
+},
+
+"007":{
+nombre:"Maria Paula Montes",
+cupos:2
+},
+
+"008":{
+nombre:"Laura Ruiz",
+cupos:2
+},
+
+"009":{
+nombre:"Jean Pierre Espinoza",
+cupos:1
+},
+
+"010":{
+nombre:"Sandra Milena Pulgarin",
+cupos:2
+},
+
+"011":{
+nombre:"Juan David Hernández",
+cupos:2
+},
+
+"012":{
+nombre:"Sebastian Hernández",
+cupos:2
+},
+
+"013":{
+nombre:"Ximena Hernández",
+cupos:2
+},
+
+"014":{
+nombre:"Anthony Quiñónez",
+cupos:2
+},
+
+"015":{
+nombre:"Víctor Hernández",
+cupos:2
+},
+
+"016":{
+nombre:"Stephanny Mira",
+cupos:2
+},
+
+"017":{
+nombre:"Maria Teresa Molina",
+cupos:1
+},
+
+"018":{
+nombre:"César Hernández",
+cupos:2
+},
+
+"019":{
+nombre:"Camila Díaz",
+cupos:2
+},
+
+"020":{
+nombre:"Jeison Suárez",
+cupos:2
+},
+
+"021":{
+nombre:"Abel Silguero",
+cupos:2
+},
+
+"022":{
+nombre:"Rodrigo Pereiro",
+cupos:1
+},
+
+"023":{
+nombre:"Daniel Rodriguez",
+cupos:1
+},
+
+"024":{
+nombre:"Sebastián Giraldo",
+cupos:2
+},
+
+"025":{
+nombre:"Miryam Rendón",
+cupos:1
+},
+
+"026":{
+nombre:"Gerardo García",
+cupos:2
+},
+
+"027":{
+nombre:"Giancarlo Moreno",
+cupos:1
+}
+
+};
